e0934c3ef5fde6426c388b96f437a645af34954e
aleks <aleksandr.kovalchukov@mail.ru>
2020-09-14 20:56:37 +0300

    initial commit

---
+++ 1.2.1.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import random
+import math
+
+class Program():
+  __interpretation_started_timestamp__ = time.time() * 1000
+
+  pi = 3.141592653589793
+
+  def execMain(self):
+    print("Привет, мир!")
+    brick.display().addLabel("Привет, ТРИК!", 50, 50)
+    brick.display().redraw()
+    script.wait(1000)
+    brick.stop()
+    return
+
+def main():
+  program = Program()
+  program.execMain()
+
+if __name__ == '__main__':
+  main()
