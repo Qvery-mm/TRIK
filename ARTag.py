414e6672ac136622ec84e91c31d9e4a5e0a4bb1e
aleks <aleksandr.kovalchukov@mail.ru>
2020-10-17 19:50:28 +0300

    ARtag decoder

--- ARTag.py
+++ ARTag.py
@@ -1,127 +1,205 @@
-import sys
-
-sys.setrecursionlimit(20000)
-start = [-1, -1]
-end = [-1, -1]
-total_height = 160
-total_width = 120
-total_treshold = 50
-
-def grayScale(sPic):
-	bufPic = [];
-	for i in range(total_height):
-		for j in range(total_width):
-			x = i * total_width + j
-			p = sPic[x]
-			r = (p & 0xff0000) >> 16	# взять значение компоненты красного
-			g = (p & 0xff00) >> 8 # взять значение компоненты зеленого
-			b = (p & 0xff) # взять значение компоненты синего
-			p = r * 0.299 + g * 0.587 + b * 0.114 # компонента Y из YUV
-			bufPic.append(p)
-	return bufPic
-
-# Бинаризация -- перевод изображения в ЧБ
-def binarization(treshold, sPic):
-	bufPic = []
-	for i in range(len(sPic)):
-		bufPic.append(255 if sPic[i] > treshold else 0);
-	return bufPic;
-
-
-def flat_to_matrix(img):
-  mat = [[0] * total_height for i in range(total_width)]
-  for i in range(total_width):
-    for j in range(total_height):
-      mat[i][j] = img[i * total_height + j]
-  return mat
+class AR():
   
-def matrix_to_flat(mat):
-  output = []
-  for i in range(total_width):
-    for j in range(total_height):
-      output.append(mat[i][j])
-  return output
+  total_height = 120
+  total_width = 160
+  total_treshold = 50
 
-visited = [[0] * total_height for i in range(total_width)] 
+  ker_n = 3
+  ker_ul = [[ 1, 1, 1],
+            [ 1, 0,-2],
+            [ 1,-2,-2]]
 
-def wave(mat, i, j, color):
-  visited[i][j] = color
-  if (i + 1 < total_width) and mat[i+1][j] == 0 and visited[i+1][j] == 0:
-    wave(mat, i+1, j, color)
-  if (j + 1 < total_height) and mat[i][j+1] == 0 and visited[i][j+1] == 0:
-    wave(mat, i, j + 1, color)
-  if (i - 1 >= 0) and mat[i-1][j] == 0 and visited[i-1][j] == 0:
-    wave(mat, i-1, j, color)
-  if (j - 1 >= 0) and mat[i][j-1] == 0 and visited[i][j-1] == 0:
-    wave(mat, i, j - 1, color)
+  ker_ur = [[ 1, 1, 1],
+            [-2, 0, 1],
+            [-2,-2, 1]]
 
-def start_wave(mat):
-  color = 1
-  for i in range(total_width):
-    for j in range(total_height):
-      if visited[i][j] == 0 and mat[i][j] == 0:
-        wave(mat, i, j, color)
-        color += 1
-  m = {}
-  for i in range(total_width):
-    for j in range(total_height):
+  ker_ll = [[ 1,-2,-2],
+            [ 1, 0,-2],
+            [ 1, 1, 1]]
+
+  ker_lr = [[-2,-2, 1],
+            [-2, 0, 1],
+            [ 1, 1, 1]]
+       
+       
+  def convolution(img, ker):
+    points = []
+    for i in range(total_height - ker_n + 1):
+      for j in range(total_width - ker_n + 1):
+        s = 0
+        for k in range(ker_n):
+          s += ker[k][0] * img[i+k][j] + ker[k][1] * img[i + k][j + 1] + ker[k][2] * img[i + k][j + 2]
+        if s == 255 * 5:
+          points.append((i, j))
+    return points
+
+    
+  def grayScale(sPic):
+    bufPic = [];
+    for i in range(total_height):
+      for j in range(total_width):
+        x = i * total_width + j
+        p = sPic[x]
+        r = (p & 0xff0000) >> 16	# взять значение компоненты красного
+        g = (p & 0xff00) >> 8 # взять значение компоненты зеленого
+        b = (p & 0xff) # взять значение компоненты синего
+        p = r * 0.299 + g * 0.587 + b * 0.114 # компонента Y из YUV
+        bufPic.append(p)
+    return bufPic
+
+
+  # Бинаризация -- перевод изображения в ЧБ
+  def binarization(treshold, sPic):
+    bufPic = []
+    for i in range(len(sPic)):
+      bufPic.append(255 if sPic[i] > treshold else 0);
+    return bufPic;
+    
+
+  def flat_to_matrix(img):
+    mat = [[0] * total_width for i in range(total_height)]
+    for i in range(total_height):
+      for j in range(total_width):
+        mat[i][j] = img[i * total_width + j]
+    return mat
+    
+    
+  def matrix_to_flat(mat):
+    output = []
+    for i in range(total_height):
+      for j in range(total_width):
+        output.append(mat[i][j])
+    return output
+
+
+  def find_vertices(Mat):
+    p = convolution(Mat, ker_ul)
+    ul = Argmin(p, lambda x: x[0] + x[1])
+    
+    p = convolution(Mat, ker_ur)
+    ur = Argmax(p, lambda x: x[1])
+    
+    p = convolution(Mat, ker_ll)
+    ll = Argmin(p, lambda x: x[1])
+
+    p = convolution(Mat, ker_lr)
+    lr = Argmax(p, lambda x: x[0] + x[1])
+    
+#    print(ul, ur, ll, lr)
+    return (ul, ur, ll, lr)
+
+
+  def Argmin(collection, func):
+    m = 1e10
+    I = ()
+    for i in collection:
+      if func(i) < m:
+        I = i
+        m = func(i)
+    return I
+    
+  def Argmax(collection, func):
+    m = -1e10
+    I = ()
+    for i in collection:
+      if func(i) > m:
+        I = i
+        m = func(i)
+    return I
+    
+  def reduce(Mat, o_array, sx, sy, dx, dy):
+    flag = True
+    tag = [[-1] * 5 for i in range(5)]
+    for i in range(5):
+      for j in range(5):
+        tag[j][i] = Mat[int(sx + i * dx[0] + j * dy[0])][int(sy + i * dx[1] + j * dy[1])] // 255
+        if (i == 0 or j == 0) and tag[j][i] != 0:
+          flag = False
+    if flag and (tag[1][1] + tag[1][3] + tag[3][1] + tag[3][3] == 1):
+      o_array.append(tag)
+  
+  def make_tag_matrix(Mat, vertices):
+    (ul, ur, ll, lr) = vertices
+    tags = []
+    
+    if len(ul) > 0 and len(ur) > 0 and len(ll) > 0:
+      dx = (ur[0] - ul[0]) / 5, (ur[1] - ul[1]) / 5
+      dy = (ll[0] - ul[0]) / 5, (ll[1] - ul[1]) / 5
+      sx, sy = ul[0] + dx[0]/2 + dy[0]/2, ul[1] + dx[1]/2 + dy[1]/2
+      AR.reduce(Mat, tags, sx, sy, dx, dy)
+     
+    if len(ul) > 0 and len(ur) > 0 and len(lr) > 0:
+      dx = (ur[0] - ul[0]) / 5, (ur[1] - ul[1]) / 5
+      dy = (lr[0] - ur[0]) / 5, (lr[1] - ur[1]) / 5 
+      sx, sy = ul[0] + dx[0]/2 + dy[0]/2, ul[1] + dx[1]/2 + dy[1]/2
+      AR.reduce(Mat, tags, sx, sy, dx, dy)
+            
+    if len(lr) > 0 and len(ur) > 0 and len(ll) > 0:
+      dx = (lr[0] - ll[0]) / 5, (lr[1] - ll[1]) / 5
+      dy = (lr[0] - ur[0]) / 5, (lr[1] - ur[1]) / 5
+      sx, sy = lr[0] - 4.5 * (dx[0] + dy[0]), lr[1] - 4.5 * (dx[1] + dy[1])
+      AR.reduce(Mat, tags, sx, sy, dx, dy)
+    
+    if len(ul) > 0 and len(ll) > 0 and len(lr) > 0:
+      dx = (lr[0] - ll[0]) / 5, (lr[1] - ll[1]) / 5
+      dy = (ll[0] - ul[0]) / 5, (ll[1] - ul[1]) / 5
+      sx, sy = ul[0] + dx[0]/2 + dy[0]/2, ul[1] + dx[1]/2 + dy[1]/2
+      AR.reduce(Mat, tags, sx, sy, dx, dy)
+      
+    return tags
+
+
+  def decode(tag):
+    t = (tag[1][2] + tag[2][1] + tag[3][2] + tag[2][3] + tag[2][2])
+    if t % 2 != 1:
+      print("checksum doesn't match")
+    s = ""
+    if tag[3][3] == 1:
+      s = str(tag[1][2]) + str(tag[2][1]) + str(tag[2][3]) + str(tag[3][2])
+    elif tag[3][1] == 1:
+      s = str(tag[2][3]) + str(tag[1][2]) + str(tag[3][2]) + str(tag[2][1]) 
+    elif tag[1][1] == 1:
+      s = str(tag[3][2]) + str(tag[2][3]) + str(tag[2][1]) + str(tag[1][2]) 
+    elif tag[1][3] == 1:
+      s = str(tag[2][1]) + str(tag[3][2]) + str(tag[1][2]) + str(tag[2][3])  
+      
+    # инвертирование бит перевод числа из двоичного в десятичный формат
+    num = int(s, 2)
+    num = ~num + 16
+    
+    return num
+  
+    
+  def get_and_decode_tag():
+    # взять кадр в формате rgb32
+    pic = getPhoto();
+    brick.display().show(pic, total_width, total_height, "rgb32");
+
+    pic = AR.grayScale(pic)
+    pic = AR.binarization(total_treshold, pic)
+
+    Mat = AR.flat_to_matrix(pic)
+    (ul, ur, ll, lr) = AR.find_vertices(Mat)
+    tags = AR.make_tag_matrix(Mat, (ul, ur, ll, lr))
+    d = {}
+    for tag in tags:
+      c = AR.decode(tag)
       try:
-        m[visited[i][j]] += 1
+        d[c] += 1
       except:
-        m[visited[i][j]] = 0
-        
-  color, count = 0, 0      
-  for i in m:
-    if i != 0 and m[i] > count:
-      color = i
-      count = m[i]
-  find_vertices(color)
+        d[c] = 1
+    m = 0
+    cipher = 0
+    for i in d:
+      if d[i] > m:
+        cipher = i
+        m = d[i]
+    print(cipher)
+ 
 
-
-def find_vertices(color):
-  global start, end
-  for i in range(total_height):
-    for j in range(total_width):
-      if visited[j][i] == color and start == [-1, -1]:
-        start = [j, i]
-      elif visited[j][i] == color:
-        end = [j, i]
-
-# взять кадр в формате rgb32
-pic = getPhoto();
-brick.display().show(pic, total_height, total_width, "rgb32");
-#script.wait(2000);
-
-pic = grayScale(pic)
-pic = binarization(total_treshold, pic)
-
-Mat = flat_to_matrix(pic)
-start_wave(Mat)
-ff = matrix_to_flat(Mat)
-
-ans = [[-1] * 5 for i in range(5)] 
-s_x, s_y = start
-e_x, e_y = end
-print(s_x, s_y)
-print(e_x, e_y)
-print()
-if(s_y < e_y):
-  dx = e_x - s_x
-  ddx = round(dx / 5)
-  dy = e_y - s_y
-  ddy = round(dy / 5)
-  tg_a = dy / dx
-  print(tg_a)
-  for i in range(5):
-    for j in range(5):
-      print((s_x + i * ddx + ddx//2), (s_y + j * ddy + ddy//2))
-      ans[i][j] = Mat[(s_x + i * ddx + ddx//2)][(s_y + j * ddy + ddy//2)]
-print(ans)
-  
-  
-
-brick.display().show(ff, total_height, total_width, "grayscale8")
-
-
-script.wait(10000)
+if __name__ == "__main__":
+  ar = AR()
+  for i in range(11):
+      AR.get_and_decode_tag()
+      
 
